4099bc19a6946db6e1abf083639eeb6b71d9431d
Abolfazl Mohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2025-12-23 13:51:24 +0330

    Create object-type.ts

---
+++ object-type.ts
@@ -0,0 +1,19 @@
+let user : {
+  id:number,
+  name:string,
+  lastName:string,
+  age:number,
+  isAdmin:boolean
+} = {
+  id:1,
+  name:"abolfazl",
+  age:17,
+  isAdmin:true
+}
+
+user.lastName = "mosheni"
+
+
+console.log(user)
+
+console.log(user.age)

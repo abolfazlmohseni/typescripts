3be150a6bdc6ea810ef332eba7ecab00d2bb4a37
Abolfazl Mohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2025-12-26 14:18:39 +0330

    Create cast-const.ts

---
+++ cast-const.ts
@@ -0,0 +1,48 @@
+let num = 12 as const
+
+// let = 13
+
+const numbers = [34,43] as const
+
+const user:{
+  id:number,
+  name:string
+}={
+  id:1,
+  name:"abolfazl"
+}
+
+function sum (num1:number,num2:number):number{
+  return num1 + num2
+}
+
+console.log(sum(...numbers))
+
+
+const favorites = ["fronend","js"]
+
+
+const users = [
+  {
+    id:1,
+    name:"abolfazl",
+    favorites
+  }
+]  as const;
+
+users[0].favorites.push("typeScript")
+
+console.log(users)
+
+let actions = [
+  {type:"ADD_TODO"},
+  {type:"REMOVE_TODO"}
+] as const
+
+// for(const action of actions ){
+//   if(action.type === "CREATE"){
+//     console.log("Create")
+//   }else{
+//     console.log("Remove")
+//   }
+// }

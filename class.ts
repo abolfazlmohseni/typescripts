ca40763ea46653a3f8b5f417b0b762e3b36ea011
abolfazlmohseni <abolfazlmohseni605@gmail.com>
2026-01-21 10:25:47 +0330

    edit class for extends

--- class.ts
+++ class.ts
@@ -13,8 +13,8 @@ class Person {
     }
 }
 
-const abolfazl = new Person("abolfazl", "mohseni", 9940564287, 17)
-const mohamad = new Person("mohamad","hasani",9965643245,23)
+// const abolfazl = new Person("abolfazl", "mohseni", 9940564287, 17)
+// const mohamad = new Person("mohamad","hasani",9965643245,23)
 // console.log(abolfazl);
 
 
@@ -24,5 +24,20 @@ const mohamad = new Person("mohamad","hasani",9965643245,23)
 // const testPerson: Person = { name: "abolfazl", lastname: "mohseni", phone: 9940564287, age: 17 }
 // console.log(testPerson);
 
-const testPersons:Person[] = [abolfazl,mohamad]
-console.log(testPersons);
+// const testPersons:Person[] = [abolfazl,mohamad]
+// console.log(testPersons);
+
+
+class Student extends Person {
+    lesson: string[]
+
+    constructor(name: string, lastname: string, phone: number, age: number, lesson: string[]) {
+        super(name, lastname, phone, age)
+        this.lesson = lesson
+    }
+}
+
+const studentone: Student = new Student("abolfazl", "mohseni", 9940564287, 17, ["algoritm", "riazi"])
+
+
+console.log(studentone);

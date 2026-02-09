88433ba65c9390d7a2b6ecd8ec277da8cee5986b
Abolfazl Mohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2026-02-09 17:39:31 +0330

    add js file

---
+++ types.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let age = 10;
+//age = "15";
+age = 15;
+let num = 10;
+// num = "test";
+num = 15;
+let text = "my text";
+// text = 65
+text = "new my text";
+let bol = false;
+bol = "true";
+bol = true;
+let list = [10, 12, 154];
+let list2 = ["test1", "test5", "test4", "test2"];
+let list3 = ["test", 12, 56, 345, "test2"];
+let userAge = 18;
+// userAge = 34
+userAge = 20;
+let anyVar = 15;
+console.log(typeof anyVar);
+anyVar = "string";
+console.log(typeof anyVar);
+anyVar = false;
+console.log(typeof anyVar);

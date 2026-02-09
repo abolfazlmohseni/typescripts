c32acb9b8df84ab6619f963c8a013615dbff810a
Abolfazl Mohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2026-02-09 17:33:48 +0330

    add convert file

---
+++ casting.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let input = "this is a text";
+let text = input; // type = unknown
+let text2 = input; // type = string
+let text = input; // type = string
+console.log(input.length);

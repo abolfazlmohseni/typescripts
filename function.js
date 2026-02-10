560518471db0f3782b36cf6adbfd2dae4ca17700
Abolfazl Mohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2026-02-10 18:59:23 +0330

    add converting file

---
+++ function.js
@@ -0,0 +1,66 @@
+"use strict";
+// function isLogin (username,password) {
+Object.defineProperty(exports, "__esModule", { value: true });
+//   if(username === "admin" && password === "admin"){
+//     return true
+//   } else{
+//     return false
+//   }
+// }
+// console.log(isLogin("admin",13235));
+// console.log(isLogin("admin","admin"))
+// function isLogin (username:string,password:string) {
+//   if(username === "admin" && password === "admin"){
+//     return true
+//   } else{
+//     return false
+//   }
+// }
+// console.log(isLogin("admin",13235)); //error
+// console.log(isLogin("admin","admin"))
+// function isLogin (username:string,password:string):boolean {
+//   if(username === "admin" && password === "admin"){
+//     return true
+//   } else{
+//     return false
+//   }
+// }
+// console.log(isLogin("admin",13235));
+// console.log(isLogin("admin","admin"))
+function isLogin(username, password) {
+    if (typeof username === "string" && typeof password === "string") {
+        if (username === "admin" && password === "admin") {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else if (typeof username === "number" && typeof password === "string") {
+        if (username === "1234" && password === "admin") {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else if (typeof username === "number" && typeof password === "number") {
+        if (username === "1234" && password === "1234") {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else if (typeof username === "string" && typeof password === "number") {
+        if (username === "admin" && password === "1234") {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}

5b0c5d1e3c0542cd1a4884de448d11aca21f163f
Abolfazl Mohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2026-02-14 13:43:31 +0330

    add files

---
+++ protect-constractor.ts
@@ -0,0 +1,50 @@
+class Person {
+
+    name: string
+    lastname: string
+    phone: number
+    age: number
+
+    //    private constructor(name: string, lastname: string, phone: number, age: number) {
+    //         this.name = name
+    //         this.lastname = lastname
+    //         this.phone = phone
+    //         this.age = age
+    //     }
+
+    protected constructor(name: string, lastname: string, phone: number, age: number) {
+        this.name = name
+        this.lastname = lastname
+        this.phone = phone
+        this.age = age
+    }
+}
+
+// const abolfazl = new Person("abolfazl", "mohseni", 9940564287, 17)
+// const mohamad = new Person("mohamad", "hasani", 9965643245, 23)
+// console.log(abolfazl);
+
+
+// const testPerson :Person = abolfazl
+// console.log(testPerson);
+
+const testPerson: Person = { name: "abolfazl", lastname: "mohseni", phone: 9940564287, age: 17 }
+console.log(testPerson);
+
+// const testPersons: Person[] = [abolfazl, mohamad]
+// console.log(testPersons);
+
+
+class Student extends Person {
+    lesson: string[]
+
+    public constructor(name: string, lastname: string, phone: number, age: number, lesson: string[]) {
+        super(name, lastname, phone, age)
+        this.lesson = lesson
+    }
+}
+
+const studentone: Student = new Student("abolfazl", "mohseni", 9940564287, 17, ["algoritm", "riazi"])
+
+
+console.log(studentone);

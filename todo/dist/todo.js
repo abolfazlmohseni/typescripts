13bbdd11765aa664baf968bd16e91182a101b6eb
Abolfazl Mohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2026-03-18 12:10:31 +0330

    add commit

---
+++ todo/dist/todo.js
@@ -0,0 +1,43 @@
+const todoValue = document.querySelector(".todo-value");
+const addTodo = document.querySelector(".add-todo");
+const clearTodo = document.querySelector(".clear-todos");
+const todoList = document.querySelector(".todoList");
+const todos = JSON.parse(localStorage.getItem("todos") || "[]");
+const addtodoHandler = (event) => {
+    event.preventDefault();
+    const newTodo = {
+        id: crypto.randomUUID(),
+        title: todoValue.value,
+        isComplate: false
+    };
+    addTodoToDom(newTodo);
+    todoValue.value = "";
+    todoValue.focus();
+    todos.push(newTodo);
+    addTodoInLocalStoreg();
+};
+const addTodoToDom = (todo) => {
+    todoList.insertAdjacentHTML("beforeend", ` 
+        <li>
+          ${todo.title}<span class="icon"><i class="fas fa-trash"></i></span>
+        </li>`);
+};
+const addTodoInLocalStoreg = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+};
+addTodo.addEventListener("click", (event) => addtodoHandler(event));
+todoValue.addEventListener("keypress", (event => {
+    if (event.code === "Enter") {
+        addtodoHandler(event);
+    }
+}));
+clearTodo.addEventListener("click", () => {
+    localStorage.clear();
+    todoList.innerHTML = "";
+});
+window.addEventListener("DOMContentLoaded", () => {
+    todos.forEach(todo => {
+        addTodoToDom(todo);
+    });
+});
+//# sourceMappingURL=todo.js.map

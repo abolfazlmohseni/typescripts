42c0c10efd4e6d31a5bd4dd839a5f628af4265f4
Abolfazl Mohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2026-03-20 23:59:27 +0330

    commit

--- todo/dist/todo.js
+++ todo/dist/todo.js
@@ -2,7 +2,20 @@ const todoValue = document.querySelector(".todo-value");
 const addTodo = document.querySelector(".add-todo");
 const clearTodo = document.querySelector(".clear-todos");
 const todoList = document.querySelector(".todoList");
-const todos = JSON.parse(localStorage.getItem("todos") || "[]");
+let todos = JSON.parse(localStorage.getItem("todos") || "[]");
+addTodo.addEventListener("click", (event) => addtodoHandler(event));
+todoValue.addEventListener("keypress", (event => {
+    if (event.code === "Enter") {
+        addtodoHandler(event);
+    }
+}));
+clearTodo.addEventListener("click", () => {
+    localStorage.clear();
+    todoList.innerHTML = "";
+});
+window.addEventListener("DOMContentLoaded", () => {
+    renderTodo();
+});
 const addtodoHandler = (event) => {
     event.preventDefault();
     const newTodo = {
@@ -18,26 +31,20 @@ const addtodoHandler = (event) => {
 };
 const addTodoToDom = (todo) => {
     todoList.insertAdjacentHTML("beforeend", ` 
-        <li>
+        <li onclick='removeTodo("${todo.id}")'>
           ${todo.title}<span class="icon"><i class="fas fa-trash"></i></span>
         </li>`);
 };
 const addTodoInLocalStoreg = () => {
     localStorage.setItem("todos", JSON.stringify(todos));
 };
-addTodo.addEventListener("click", (event) => addtodoHandler(event));
-todoValue.addEventListener("keypress", (event => {
-    if (event.code === "Enter") {
-        addtodoHandler(event);
-    }
-}));
-clearTodo.addEventListener("click", () => {
-    localStorage.clear();
+const removeTodo = (id) => {
+    todos = todos.filter(todo => todo.id !== id);
+    addTodoInLocalStoreg();
     todoList.innerHTML = "";
-});
-window.addEventListener("DOMContentLoaded", () => {
-    todos.forEach(todo => {
-        addTodoToDom(todo);
-    });
-});
+    renderTodo();
+};
+const renderTodo = () => {
+    todos.forEach(todo => addTodoToDom(todo));
+};
 //# sourceMappingURL=todo.js.map
